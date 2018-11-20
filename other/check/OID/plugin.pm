9f98717c18082479e31575d64fb1802a1759a60f
Rod'n <41026476+rodeur2225@users.noreply.github.com>
2018-11-21 09:58:15 +1100

    Update plugin.pm

---
+++ other/check/OID/plugin.pm
@@ -0,0 +1,31 @@
+#
+#  Rod'n
+# 21/11/18
+
+package other::check::OID
+
+use strict;
+use warnings;
+use bas qw(centreon::plugins::script_simple);
+
+sub new {
+  my ($class, %options) = @_;
+  my $self = $class->SUPER::new(package -> __PACKAGE__, %options);
+  bless $self,$class;
+  
+  $self->{version} = '0.1';
+  %{$self->[modes}} = (
+    'string' => 'other::check::OID::string',
+    );
+   return $self;
+}
+
+1;
+
+__END__
+
+=head1 PLUGIN DESCRIPTION
+
+Check OID
+
+=cut
