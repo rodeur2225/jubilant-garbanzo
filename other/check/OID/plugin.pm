c4a67624603b0d32ea02b46f30dcda3e46617996
Rod'n <41026476+rodeur2225@users.noreply.github.com>
2018-11-21 10:12:01 +1100

    Update plugin.pm

--- other/check/OID/plugin.pm
+++ other/check/OID/plugin.pm
@@ -10,7 +10,7 @@ use base qw(centreon::plugins::script_simple);
 
 sub new {
   my ($class, %options) = @_;
-  my $self = $class->SUPER::new(package -> __PACKAGE__, %options);
+  my $self = $class->SUPER::new(package => __PACKAGE__, %options);
   bless $self,$class;
   
   $self->{version} = '0.1';
