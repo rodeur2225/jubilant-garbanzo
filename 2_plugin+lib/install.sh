9411b2bb07f7886c972b22a1d958ed406d60b5dd
root <root@DESKTOP-39095TG.localdomain>
2018-12-12 08:35:15 +1100

    ajout d'un installateur pour les plugins perso et les scripts et les plugins nagios

---
+++ 2_plugin+lib/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo  "installation des plugins centreon open source + ajout de plugin perso + installation des script necessaire + installation des plugins nagios"
+echo "installation de centreon plugins"
+git clone http://github.com/rodeur2225/centreon-plugins.git
+if [ $? != 0 ];then
+	echo "errue d'installation verifier votre conexion internet"
+	exit 2
+fi
+false
+chmod 775 ./centreon-plugins/centreon_plugins.pl
+echo "installation de du package de plugin other (plugin perso)"
+mkdir ./centreon-plugins/other
+mv ./2_other/* ./centreon-plugins/other
+echo -e "les plugins other sont installer dans centreon-plugins \nles voici :"
+perl ./centreon-plugins/centreon_plugin.pl --list-plugin | grep other
+echo "tapez <entrer> pour continuer"
+read touch
+mv ./centreon-plugins /usr/lib/centreon/plugins
+echo "centreon-plugins est installe dans /usr/lib/centreon/plugins"
+echo "installation des scripts et plugins nagios"
+mv ./3_lib/* ./usr/lib
+echo -e "les scripts sont installé dans /usr/lib/centreon_sh\nles plugins nagios sont installés dans /usr/lib/nagios"
+echo "installation terminé n'oublier pas de supprimer jubilant-garbanzo quand vous aurez fini"
