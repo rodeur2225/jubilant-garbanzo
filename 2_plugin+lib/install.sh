5977455a974cce3af8ce1487035f6f84b9b0917b
BananaLord <you@example.com>
2018-12-14 09:52:23 +1100

    modif droit des install.sh

--- 2_plugin+lib/install.sh
+++ 2_plugin+lib/install.sh
@@ -9,15 +9,15 @@ fi
 false
 chmod 775 ./centreon-plugins/centreon_plugins.pl
 echo "installation de du package de plugin other (plugin perso)"
-mv ./2_other ./centreon-plugins/other
+mv -f ./2_other ./centreon-plugins/other
+mv -f ./centreon-plugins /usr/lib/centreon/plugins
 echo -e "les plugins other sont installer dans centreon-plugins \nles voici :"
 perl ./centreon-plugins/centreon_plugins.pl --list-plugin | grep other
 echo "tapez <entrer> pour continuer"
 read touch
-mv ./centreon-plugins /usr/lib/centreon/plugins
 echo "centreon-plugins est installe dans /usr/lib/centreon/plugins"
 echo "installation des scripts et plugins nagios"
-mv ./3_lib/* /usr/lib
+mv -f ./3_lib/* /usr/lib
 echo -e "les scripts sont installé dans /usr/lib/centreon_sh\nles plugins nagios sont installés dans /usr/lib/nagios"
 echo "installation terminé n'oublier pas de supprimer jubilant-garbanzo quand vous aurez fini"
 cd
