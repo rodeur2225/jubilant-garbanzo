aa0c1ecbe08887b4ff8bfaead493711a3824e282
Rod'n <41026476+rodeur2225@users.noreply.github.com>
2018-12-14 11:35:20 +1100

    Update install.sh

--- 2_plugin+lib/install.sh
+++ 2_plugin+lib/install.sh
@@ -2,6 +2,7 @@
 echo  "installation des plugins centreon open source + ajout de plugin perso + installation des script necessaire + installation des plugins nagios"
 echo "installation de centreon plugins"
 if [ ! -d /usr/lib/centreon/plugins/centreon-plugins ];then
+#on recupere le git centreon-plugins et on l'integre a centreon avec les plugin de jubilant-garbanzo
 	git clone http://github.com/rodeur2225/centreon-plugins.git
 	if [ $? != 0 ];then
 		echo "errue d'installation verifier votre conexion internet"
@@ -10,11 +11,14 @@ if [ ! -d /usr/lib/centreon/plugins/centreon-plugins ];then
 	false
 	chmod 775 ./centreon-plugins/centreon_plugins.pl
 	echo "installation de du package de plugin other (plugin perso)"
+	mkdir ./centreon-plugins/other
 	mv -f ./2_other/* ./centreon-plugins/other
 	mv -f ./centreon-plugins /usr/lib/centreon/plugins
 else
+#si centreon-plugins est deja installé on se content d'y rahouter les plugins
 	if [ -d /usr/lib/centreon/plugins/centreon-plugins/other ];then
 		rm -rf /usr/lib/centreon/plugins/centren-plugins/other
+		mkdir /usr/lib/centreon/plugins/centren-plugins/other
 		mv -f ./2_other /usr/lib/centreon/plugins/centreon-plugins/other
 	fi		
 fi
