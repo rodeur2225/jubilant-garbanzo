cfc37b10a9ab1922890083da84cf04037b543aef
Rod'n <41026476+rodeur2225@users.noreply.github.com>
2018-12-20 10:24:52 +1100

    Update install.sh

--- 3_Nagvis/install.sh
+++ 3_Nagvis/install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+echo "verification de l'installation de mlocate"
 yum install -y -q mlocate
-echo "verfification de l'intalation de nagvis"
+echo "verfification de l'instalation de nagvis"
+sleep 2
 $pwd=`locate --regex /usr/share/nagvis$
 if [ ! $pwd == "/usr/share/nagvis" ];then
-  echo "nagvis n'est pas installé fin de l'installation"
+  echo "nagvis n'est pas installé fin de l'installation (voire les ateleir de kermith pour l'installation)"
   exit 2
 fi
-echo "nagvis est present exportation des icon vers nagvis"
+echo "nagvis est present exportation des icones vers nagvis"
+sleep2
 mv ./*_*/*_* /usr/share/nagvis/share/userfiles/images/iconsets
 echo "vos icones sont installé les voici"
+sleep 2
 ls /usr/share/nagvis/share/userfiles/images/iconsets
 echo "appuyer sur <entrer> pour quitter l'installation"
 read touche
