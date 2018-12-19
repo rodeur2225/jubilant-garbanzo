f977f8d0b78ead679c31bda7bd8e2b77bc167feb
Rod'n <41026476+rodeur2225@users.noreply.github.com>
2018-12-20 10:20:43 +1100

    Update install.sh

--- 2_plugin+lib/install.sh
+++ 2_plugin+lib/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo  "installation des plugins centreon open source + ajout de plugin perso + installation des script necessaire + installation des plugins nagios"
-echo "installation de centreon plugins"
+sleep 2
+echo "installation de du package de plugin other"
 if [ ! -d /usr/lib/centreon/plugins/centreon-plugins ];then
 #on recupere le git centreon-plugins et on l'integre a centreon avec les plugin de jubilant-garbanzo
+	echo "centreon-plugin inexistant installation"
 	git clone http://github.com/rodeur2225/centreon-plugins.git
 	if [ $? != 0 ];then
 		echo "errue d'installation verifier votre conexion internet"
@@ -10,7 +12,6 @@ if [ ! -d /usr/lib/centreon/plugins/centreon-plugins ];then
 	fi
 	false
 	chmod 775 ./centreon-plugins/centreon_plugins.pl
-	echo "installation de du package de plugin other (plugin perso)"
 	mkdir ./centreon-plugins/other
 	mv -f ./2_other/* ./centreon-plugins/other
 	mv -f ./centreon-plugins /usr/lib/centreon/plugins
@@ -28,14 +29,20 @@ perl /usr/lib/centreon/plugins/centreon-plugins/centreon_plugins.pl --list-plugi
 echo "tapez <entrer> pour continuer"
 read touch
 echo "centreon-plugins est installe dans /usr/lib/centreon/plugins"
+sleep 3
 echo "installation des scripts et plugins nagios"
+sleep 3
 if [ -d /usr/lib/centreon_sh ];then
+	echo "fichier existant remplacement"
+	sleep 2
 	rm -rf /usr/lib/centreon_sh
 fi
-if [ -d /usr/lib/nagios ];then 
+if [ -d /usr/lib/nagios ];then
+	echo "fichier existant remplacement"
+	sleep 2
 	rm -rf /usr/lib/nagios
 fi
 mv ./3_lib/* /usr/lib
 echo -e "les scripts sont installé dans /usr/lib/centreon_sh\nles plugins nagios sont installés dans /usr/lib/nagios"
+sleep 3
 echo "installation terminé n'oublier pas de supprimer jubilant-garbanzo quand vous aurez fini"
-cd
